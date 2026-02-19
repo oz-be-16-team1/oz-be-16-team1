234c6399db4b61d0988f0ca096e4b612e3f61633
Brandon Hyun <>
2026-02-19 16:57:47 +0900

    feat: 패키지 설치 uv 명령어 수정

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,13 +1,17 @@
 from .base import *
 import os
 import environ
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 DEBUG = env.bool("DEBUG", default=True)
 
 # .env에서 가져오거나 기본값 설정
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1", "web"])
 
+print(f"DEBUG: DB_HOST is {env('DB_HOST', default='NOT FOUND')}")   #debug용, 잘되면 지우거나 주석
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
