12f6ae52460477301c7ab5beb1251f05a48ef58d
Brandon Hyun <>
2026-02-19 13:38:37 +0900

    feat: settings.py 개발환경, 배포환경으로 분리

---
+++ config/settings/dev.py
@@ -0,0 +1,24 @@
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+# 개발 환경에서는 로깅을 좀 더 상세하게
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
