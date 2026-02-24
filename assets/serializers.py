8196ae2a1cd3f34466a64fe297145d86db9d2c96
Justman-yzz <a01088744395@gmail.com>
2026-02-24 14:19:13 +0900

    ✨ feat: assets 앱 ERD 기반 1차 구현

---
+++ assets/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from assets.models import Asset
+
+
+class AssetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Asset
+        fields = "__all__"
+        read_only_fields = ("id", "created_at")
---
+++ assets/views.py
@@ -0,0 +1,15 @@
+from rest_framework import permissions, viewsets
+
+from assets.models import Asset
+from assets.serializers import AssetSerializer
+
+
+class AssetViewSet(viewsets.ModelViewSet):
+    serializer_class = AssetSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Asset.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
