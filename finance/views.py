3933341cd622937136c47db3037b5f9aa4f182c0
PJG8806 <jinguy39@gmail.com>
2026-02-25 17:02:46 +0900

    🐛 fix :거래정보 테이블, 마이그레이션 출력 및 주석 추가

--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+from rest_framework.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
@@ -7,19 +9,19 @@ from finance.models import Transaction, FixedExpense
 from finance.serializers import TransactionSerializer, FixedExpenseSerializer
 
 
+# 거래 정보
 class TransactionListCreateAPIView(viewsets.ModelViewSet):
     serializer_class = TransactionSerializer
     permission_classes = [IsAuthenticated]
 
+    # 거래 정보 출력
     def get_queryset(self):
         user = self.request.user
 
         # 조회자가 부모일 경우
         if user.role == "PARENT":
-            user_children = user.parent.values_list("id", flat=True)
-
             return (
-                Transaction.objects.filter(user__in=list(user_children) + [user.id])
+                Transaction.objects.filter(Q(user=user) | Q(user__parent=user))
                 .select_related("asset")
                 .order_by("-created_at")
             )
@@ -27,11 +29,22 @@ class TransactionListCreateAPIView(viewsets.ModelViewSet):
         else:
             # 부모가 아니라면
             return (
-                Transaction.objects.filter(user=self.request.user)
+                Transaction.objects.filter(user=user)
                 .select_related("asset")
                 .order_by("-created_at")
             )
 
+    # 수정 삭제 상세조회 등 단일 객체 하나를 건드릴때 적용
+    def get_object(self):
+        obj = super().get_object()
+        user = self.request.user
+
+        if user.role == "PARENT" and obj.user != user:
+            raise PermissionDenied("수정은 작성자만 가능합니다.")
+
+        return obj
+
+    # 거래 정보 추가
     def perform_create(self, serializer):
         asset_id = self.request.data.get("asset")
 
@@ -47,18 +60,26 @@ class TransactionListCreateAPIView(viewsets.ModelViewSet):
         else:
             serializer.save(user=self.request.user)
 
+    # 거래 정보 수정
     def perform_update(self, serializer):
         serializer.save(user=self.request.user)
 
 
+# 정기 구독
 class FixedExpenseListCreateAPIView(viewsets.ModelViewSet):
     serializer_class = FixedExpenseSerializer
     permission_classes = [IsAuthenticated]
 
+    # 정기 구독 출력
     def get_queryset(self):
         return FixedExpense.objects.filter(user=self.request.user).order_by(
             "-payment_day"
         )
 
+    # 정기 구독 추가
     def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    # 정기 구독 수정
+    def perform_update(self, serializer):
         serializer.save(user=self.request.user)
