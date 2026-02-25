ee82364b54c6d15464fe3d491637bcf925d6b6d3
PJG8806 <jinguy39@gmail.com>
2026-02-25 17:47:27 +0900

    🐛 fix : migrations 불일치 수정

--- finance/tests.py
+++ finance/tests.py
@@ -1,1 +1,18 @@
-# Create your tests here.
+from django.test import TestCase
+
+
+class TransactionTest(TestCase):
+    def setUp(self):
+        self.test_transaction = {
+            "amount": "1000",
+            "store_name": "test",
+            "category": "수입",
+            "category_middle": "기타",
+            "guitar": "",
+            "is_confirmed": True,
+            "importance": 1,
+            "memo": "test",
+            "is_fixed_expense": False,
+            "create_at": "2026-02-25T14:09:06.250530+09:00",
+            "real_date": "2026-02-25T14:09:06.250530+09:00",
+        }
