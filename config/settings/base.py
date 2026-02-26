135f1898a88bebc2eae3204e23b01e0db3ae0f7f
Brandon Hyun <>
2026-02-26 13:32:33 +0900

    feat:swagger 추가

--- config/settings/base.py
+++ config/settings/base.py
@@ -128,3 +128,11 @@ LOGGING = {
         "level": "INFO",
     },
 }
+
+SWAGGER_SETTINGS = {
+    "SECURITY_DEFINITIONS": {
+        "Bearer": {"type": "apiKey", "name": "Authorization", "in": "header"}
+    },
+    "USE_SESSION_AUTH": False,
+    "JSON_EDITOR": True,
+}
